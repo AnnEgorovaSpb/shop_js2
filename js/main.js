5ab5c57b4135d43ffa760b225664d203c726f0b4
Anna Egorova <see-you@yandex.ru>
2020-05-26 00:17:45 +0300

    lesson_02 подготовлен к lesson_3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductList { 
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
         this.allProducts = [];
-        this.allProductsPrice = 0; // переменная со значением общей стоимости
-        this._fetchProduct();
-        this.render();
-        this.getTotalPrice(); //  метод, определяющий суммарную стоимость всех товаров
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
     }
 
-    _fetchProduct() {
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ]
+    _getProducts() {
+        return fetch (`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
 
     render() {
@@ -25,19 +31,14 @@ class ProductList {
             this.allProducts.push(productObj);
             block.insertAdjacentHTML('beforeend', productObj.render());
         }
-        
-    }
-    /* метод, определяющий суммарную стоимость всех товаров */
-    getTotalPrice() {
-        this.goods.forEach(productPrice => this.allProductsPrice += productPrice.price);
     }
 }
 
 class ProductItem {
     constructor(product, img = "https://placehold.it/200x150") {
-        this.title = product.title;
+        this.title = product.product_name;
         this.price = product.price;
-        this.id = product.id;
+        this.id = product.id_product;
         this.img = img;
     }
     render() {
@@ -51,23 +52,3 @@ class ProductItem {
 }
 
 let list = new ProductList();
-
-/* класс для корзины товаров  */
-class Basket {
-    constructor() {}
-
-    /* addToBasket() {метод добавляет выбранный товар в корзину} */
-
-    /* getBasketPrice() {метод считает общую стоимость товаров в корзине} */
-
-    /* deleteBasketProduct() {метод удаляет выбранный товар из корзины} */
-}
-
-/* класс для элементов корзины */
-class BasketProduct {
-    constructor() {}
-    
-    /* incrementBasketProduct() {метод увеличевает количество выбранного товара на единицу} */
-
-    /* decrementBasketProduct() {метод уменьшает количество выбранного товара на единицу} */
-}
